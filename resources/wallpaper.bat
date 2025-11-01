da4e9b939862aba760e26d02767fcb006f39bcbc
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-01 11:30:28 +0100

    Create wallpaper.bat

---
+++ resources/wallpaper.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+REG ADD "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
+
+REG ADD "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+
+REG ADD "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d "2" /f
+REG ADD "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d "0" /f
+
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 0 /f
+
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
+
+TASKKILL /F /IM explorer.exe
+start explorer.exe
+
+endlocal
+exit
